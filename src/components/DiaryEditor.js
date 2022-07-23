2fc3d48b1a1ce8db1a89ffd4f02455b3a0815226
Jeongyujae <morrisrt6851@naver.com>
2022-07-23 10:06:40 +0900

    Diary page

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -5,50 +5,11 @@ import MyHeader from "./MyHeader"
 import MyButton from "./MyButton";
 import EmotionItem from "./EmotionItem";
 import {DiaryDispatchContext} from "./../App.js"
+import { getStringDate } from "../util/date";
+import { emotionList } from "../util/emotion";
 
-const emotionList = [
-    {
-        emotion_id:1,
-        emotion_img:process.env.PUBLIC_URL+`/assets/emotion1.png`,
-        emotion_descript: "최고"
-    },
-    {
-        emotion_id:2,
-        emotion_img:process.env.PUBLIC_URL+`/assets/emotion2.png`,
-        emotion_descript: "좋음"
-    },
-    {
-        emotion_id:3,
-        emotion_img:process.env.PUBLIC_URL+`/assets/emotion3.png`,
-        emotion_descript: "보통"
-    },
-    {
-        emotion_id:4,
-        emotion_img:process.env.PUBLIC_URL+`/assets/emotion4.png`,
-        emotion_descript: "별로"
-    },
-    {
-        emotion_id:5,
-        emotion_img:process.env.PUBLIC_URL+`/assets/emotion5.png`,
-        emotion_descript: "최악"
-    }
-]
 
-export const getStringDate= (date) => {
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
-    if (month < 10) {
-      month = `0${month}`;
-    }
-    
-    if (day < 10) {
-      day = `0${day}`;
-    }
-  
-    return `${year}-${month}-${day}`;
-  
-};
+
 
 
 
---
+++ src/util/emotion.js
@@ -0,0 +1,27 @@
+export const emotionList = [
+    {
+        emotion_id:1,
+        emotion_img:process.env.PUBLIC_URL+`/assets/emotion1.png`,
+        emotion_descript: "최고"
+    },
+    {
+        emotion_id:2,
+        emotion_img:process.env.PUBLIC_URL+`/assets/emotion2.png`,
+        emotion_descript: "좋음"
+    },
+    {
+        emotion_id:3,
+        emotion_img:process.env.PUBLIC_URL+`/assets/emotion3.png`,
+        emotion_descript: "보통"
+    },
+    {
+        emotion_id:4,
+        emotion_img:process.env.PUBLIC_URL+`/assets/emotion4.png`,
+        emotion_descript: "별로"
+    },
+    {
+        emotion_id:5,
+        emotion_img:process.env.PUBLIC_URL+`/assets/emotion5.png`,
+        emotion_descript: "최악"
+    }
+]
